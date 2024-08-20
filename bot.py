e8efd0f63837f7bd79acc2317459e9a27a36919c
ShootkA4 <techsupport23@rambler.ru>
2024-08-20 17:05:57 +0300

    First alpha

---
+++ bot.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types 
+from aiogram.filters.command import Command
+from aiogram.types import Message
+logging.basicConfig(level=logging.INFO)
+token_bot="7533396258:AAFSVT5bFhM13xy_2FAAtTa-TDXGnxbHokc"
+
+async def echo(message: Message):
+    await message.answer(message.text)
+
+
+
+async def start():
+    bot=Bot(token=token_bot)
+    dp=Dispatcher()
+
+    dp.message.register(echo)
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+if __name__=='__main__':
+    asyncio.run(start())
+
